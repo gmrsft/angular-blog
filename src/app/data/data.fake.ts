04d1d3255b2141a60656a19380b94fc0239844e0
Guilherme Mathias Ribeiro <admguimat@gmail.com>
2024-01-30 11:44:50 -0300

    content created

---
+++ src/app/data/data.fake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "saiu um novo filme",
+    "description": "ola",
+    "photoCover":"https://images.unsplash.com/photo-1698685247855-078092c12b1b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D"
+  },
+  {
+    "id":"2",
+    "title": "saiu um novo filme",
+    "description": "ola",
+    "photoCover":"https://images.unsplash.com/photo-1704152282392-3cf7bdc2bce3?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDl8Q0R3dXdYSkFiRXd8fGVufDB8fHx8fA%3D%3D"
+  },
+]
